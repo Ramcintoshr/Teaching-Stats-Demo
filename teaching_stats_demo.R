2faff1fed218e19897be9aee0501bb220aaa85e1
Ramcintoshr <ramcintoshr@gmail.com>
2025-06-07 14:01:12 -0500

    Update teaching_stats_demo.R

--- teaching_stats_demo.R
+++ teaching_stats_demo.R
@@ -1,16 +1,9 @@
 # teaching_stats_demo.R
 
-# Simulated example for teaching t-tests and regression
-
 library(tidyverse)
 
-# Simulated data: cholesterol levels by exercise group
-set.seed(123)
-data <- tibble(
-  group = rep(c("Low Activity", "High Activity"), each = 30),
-  cholesterol = c(rnorm(30, 215, 20), rnorm(30, 190, 18)),
-  age = sample(25:60, 60, replace = TRUE)
-)
+# Load simulated dataset
+data <- read_csv("simulated_health_data.csv")
 
 # t-test between groups
 t_test_result <- t.test(cholesterol ~ group, data = data)
